b62e96baf9541c766991062bb01cb51ae77930fe
A-d-a-m <adam3928@mail.ru>
2023-08-30 16:35:22 +0200

    initial commit

--- conftest.py
+++ conftest.py
@@ -1,8 +1,35 @@
-import os
+import pytest
 
-CURRENT_FILE_PATH = os.path.abspath(__file__)
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
-PROJECT_ROOT_PATH = os.path.dirname(CURRENT_FILE_PATH)
-TESTS_ROOT_PATH = os.path.join(PROJECT_ROOT_PATH, 'tests')
-RESOURCES_DIR = os.path.join(TESTS_ROOT_PATH, 'resources')
+from utils import attach
 
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = "100.0"
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
