4be8a49b95f00f448444bd715f4fc89b39c020cb
A-d-am <30774921+A-d-am@users.noreply.github.com>
2023-08-30 16:21:09 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,35 @@
 import pytest
-from selene import browser
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
 from utils import attach
 
-@pytest.fixture(scope='function', autouse=True)
-def driver_management():
-    browser.config.base_url = 'https://demoqa.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = "100.0"
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
-    yield
-    attach.add_html(browser)
-    attach.add_logs(browser)
+    browser = Browser(Config(driver))
+    yield browser
+
     attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
     attach.add_video(browser)
 
     browser.quit()
