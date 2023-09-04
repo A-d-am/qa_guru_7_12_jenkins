424d491560fae4a6c32e9a4178aad16d2800f3e8
A-d-a-m <adam3928@mail.ru>
2023-09-04 16:23:52 +0200

    changed to chrome

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ def setup_browser(request):
     browser_version = "100.0"
     options = Options()
     selenoid_capabilities = {
-        "browserName": "firefox",
+        "browserName": "chrome",
         "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
